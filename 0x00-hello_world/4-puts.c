3d5ae840b79918e9695fff4d8f2ea296254c510f
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-10 16:29:07 -0700

    C programs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - Program using puts function in C to print a message
+ * Return: 0 (program has terminated successfully.)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
