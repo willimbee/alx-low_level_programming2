5f3df4e2c4f95aa6eefd22e0caf386539ffe65e0
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-10 16:43:20 -0700

    fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Printing text without using printf or puts
  * Return: 1 (Success)
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-int length = strlen(message);
-write(2, message "\n", length);
-return (1);
+ const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t bytes_written = write(STDERR_FILENO, message, 59); // 59 is the length of the message
+
+    if (bytes_written == -1) {
+        perror("write");
+        return 1;
+    }
+
+    return 1;
 }
