6720f5a4c40f9940174ebf01c51780bbc3d43a16
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-10 17:12:24 -0700

    101-fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t bytes_written = write(STDERR_FILENO, message, 59);
 
 	if (bytes_written == -1)
