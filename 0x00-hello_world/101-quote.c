e2745125984cf3c34923a06cb9002d9df05d0c0f
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-10 18:38:21 -0700

    101-fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 /**
  * main - Printing text without using printf or puts
@@ -7,13 +5,6 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t bytes_written = write(STDERR_FILENO, message, 59);
-
-    if (bytes_written == -1)
-    {
-        return (1);
-    }
-
-    return (1);
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
