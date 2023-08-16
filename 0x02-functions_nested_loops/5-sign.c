95c94fac89ab1cd7518fa3b0fa71725d503f333a
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-15 18:44:41 -0700

    5-fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,13 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
-}
-		_putchar(48);
+	  {
+		_putchar('0');
 		return (0);
 	} else if (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
+	}
+	return (0);
 }
