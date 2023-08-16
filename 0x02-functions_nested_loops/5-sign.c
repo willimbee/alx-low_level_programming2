df090ea2845689007405b904350596c7a5ba63aa
Oka William TCHAGBONON <williamtchagbonon2002@gmail.com>
2023-08-15 18:55:59 -0700

    5-fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -13,11 +12,13 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n == 0)
 	  {
 		_putchar('0');
 		return (0);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
